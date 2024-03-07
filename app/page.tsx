213db7e811bf508b639b6761a4545ab5ae7bb2ca
remy.baran@protonmail.com <remy.baran@protonmail.com>
2024-03-07 18:08:52 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>New title 2</h1>
+      <h1>New title 2 br 6</h1>
     </main>
   );
 }
